cc5dcc89434916f17eef6778c6989cef75037b1b
Abdul Shakoor <abduldev1786@gmail.com>
2026-08-30 19:37:24 +0500

    global Error handler is add and also make some changes in my root files

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import notFound from "../middlewares/notFoundMiddleware.js";
+import errorHandler from "../middlewares/globalErrorHandler.js";
+import ApiResponse from "../utils/ApiResponse.js";
 
 dotenv.config();
 
@@ -9,5 +12,14 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json(new ApiResponse(200, null, "Server is running successfully"));
+});
+
+app.use(notFound);
+app.use(errorHandler);
 
 export default app;
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,9 +3,6 @@ const PORT = process.env.PORT || 8000;
 import connectDB from "../config/db.connection.js";
 
 connectDB();
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port https://localhost:${PORT}`);
